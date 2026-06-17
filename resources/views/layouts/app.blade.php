71df4f0490766e46c770c2e36ec6a3d234c6dd24
tegarajah24 <tegarrifai1410@gmail.com>
2026-06-17 13:12:31 +0700

    menghapus button notification dan chat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -195,20 +195,6 @@
 
                     {{-- Right: Actions --}}
                     <div class="flex items-center gap-1">
-                        {{-- Chat (placeholder) --}}
-                        <button title="Chat" class="p-2 rounded-lg text-slate-500 hover:text-blue-600 hover:bg-slate-100/50 transition-colors">
-                            <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"/>
-                            </svg>
-                        </button>
-
-                        {{-- Notification (placeholder) --}}
-                        <button title="Notifikasi" class="p-2 rounded-lg text-slate-500 hover:text-blue-600 hover:bg-slate-100/50 transition-colors relative">
-                            <svg class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"/>
-                            </svg>
-                        </button>
-
                         {{-- Profile Dropdown --}}
                         <div class="ms-2 relative">
                             <x-dropdown align="right" width="48">
